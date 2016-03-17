16b2a5be3c5f7187adca37d71cef2b4af3147ee7
Justin Gray <justin.s.gray@gmail.com>
2016-03-17 08:59:41 -0400

    geom derivs

--- geometry.py
+++ geometry.py
@@ -16,9 +16,18 @@ class GeometryMesh(Component):
         self.add_param('twist', val=numpy.zeros((n)))
         self.add_output('mesh', val=numpy.zeros((2, n, 3)))
 
-        self.fd_options['force_fd'] = True
+        # self.fd_options['force_fd'] = True
         self.fd_options['form'] = "complex_step"
         self.fd_options['extra_check_partials_form'] = "central"
+
+        ones = numpy.ones(n, dtype="int")
+        idx = [ones,range(n),2*ones]
+        self.twist_idx_flat = (numpy.ravel_multi_index(idx, (2,n,3)), range(n))
+
+        J = {}
+        J['mesh','twist'] = numpy.zeros((2*n*3, n))
+        J['mesh','twist'][self.twist_idx_flat] = -1.
+        self.J = J
 
     def solve_nonlinear(self, params, unknowns, resids):
         n = self.n
@@ -32,3 +41,7 @@ class GeometryMesh(Component):
 
         mesh[1, :, 2] = -twist
         unknowns['mesh'] = mesh
+
+    def linearize(self, params, unknowns, resids): 
+
+        return self.J
