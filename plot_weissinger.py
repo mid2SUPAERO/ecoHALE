6ca9202463286de3ce66f32273b4e216e872e483
John Jasa <johnjasa11@gmail.com>
2016-03-18 00:32:51 -0400

    added wing/tube options to 3D viz

--- plot_weissinger.py
+++ plot_weissinger.py
@@ -1,18 +1,27 @@
 from __future__ import division
 import sqlitedict
 import numpy
+import sys
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 
-db = sqlitedict.SqliteDict('aerostruct.db', 'openmdao')
+"""
+User-set options:
+"""
+db_name = 'aerostruct.db'
+iteration = -1
+show_wing = True
+show_tube = True
+
+db = sqlitedict.SqliteDict(db_name, 'openmdao')
 
 cl = []
 cd = []
 twist = []
 mesh = []
-# r = []
-# t = []
+r = []
+t = []
 
 for case_name, case_data in db.iteritems():
     if "metadata" in case_name or "derivs" in case_name:
@@ -22,10 +31,11 @@ for case_name, case_data in db.iteritems():
     cd.append(case_data['Unknowns']['CD'])
     twist.append(case_data['Unknowns']['twist'])
     mesh.append(case_data['Unknowns']['mesh'])
-    # r.append(case_data['Unknowns']['r'])
-    # t.append(case_data['Unknowns']['t'])
-
-mesh0 = mesh[0]
+    try:
+        r.append(case_data['Unknowns']['r'])
+        t.append(case_data['Unknowns']['t'])
+    except:
+        pass
 
 def plot_wing(mesh, r=None, t=None, tube_only=False):
     fig = plt.figure()
@@ -40,15 +50,17 @@ def plot_wing(mesh, r=None, t=None, tube_only=False):
         ax.plot_wireframe(x, y, z, rstride=1, cstride=1, color='k')
     max_dim = numpy.max(mesh)
 
-    chord = mesh[1, :, 0] - mesh[0, :, 0]
+    chords = mesh[1, :, 0] - mesh[0, :, 0]
 
     if r != None and t != None:
+        r = numpy.hstack((r, r[-1]))
+        t = numpy.hstack((t, t[-1]))
         num_circ = 40
         fem_origin = 0.35
         p = numpy.linspace(0, 2*numpy.pi, num_circ)
         R, P = numpy.meshgrid(r, p)
         X, Z = R*numpy.cos(P), R*numpy.sin(P)
-        X[:] += fem_origin * chord
+        X[:] += fem_origin * chords
         Y = numpy.empty(X.shape)
         Y[:] = numpy.linspace(0, max_dim, n)
         colors = numpy.empty(X.shape)
@@ -58,11 +70,15 @@ def plot_wing(mesh, r=None, t=None, tube_only=False):
     ax.auto_scale_xyz([-max_dim/2, max_dim/2], [0, max_dim], [-max_dim/2, max_dim/2])
     plt.show()
 
-n = mesh0.shape[1]
+mesh0 = mesh[iteration]
 
-r = numpy.ones(n) * .5
-t = numpy.arange(n) * .2
-
-plot_wing(mesh0)
-plot_wing(mesh0, r, t, tube_only=True)
-plot_wing(mesh0, r, t)
+if show_wing and not show_tube:
+    plot_wing(mesh0)
+if show_tube and not show_wing:
+    r0 = r[iteration]
+    t0 = t[iteration]
+    plot_wing(mesh0, r, t, tube_only=True)
+if show_tube and  show_wing:
+    r0 = r[iteration]
+    t0 = t[iteration]
+    plot_wing(mesh0, r, t)
