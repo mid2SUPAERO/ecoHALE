f70730c7358ba7273c098d7694a8e5746aa948a6
Justin Gray <justin.s.gray@gmail.com>
2016-03-17 22:59:59 -0400

    foo

--- run_weissinger.py
+++ run_weissinger.py
@@ -10,7 +10,7 @@ from weissinger import WeissingerGroup
 
 num_y = 5
 # span = 232.02
-span = 40.
+span = 100.
 chord = 10.
 
 v = 200.
@@ -24,7 +24,7 @@ root = Group()
 des_vars = [
     ('twist', numpy.zeros(num_y)), 
     ('span', span),
-    ('chord', numpy.array([.1, 5., 10, 5., .1])), # numpy.ones(num_y)*chord
+    ('chord', numpy.ones(num_y)*chord),
     ('v', v),
     ('alpha', alpha), 
     ('rho', rho), 
@@ -76,9 +76,11 @@ prob.driver.options['disp'] = True
 
 prob.driver.add_desvar('twist',lower=numpy.ones((num_y)) * -10.,
                        upper=numpy.ones((num_y)) * 10.)
-prob.driver.add_desvar('alpha', lower=-10., upper=10., scaler=100)
+# prob.driver.add_desvar('chord',lower=numpy.ones((num_y)) * 1,
+#                        upper=numpy.ones((num_y)) * 12)
+prob.driver.add_desvar('alpha', lower=-10., upper=10.)
 prob.driver.add_objective('CD')
-prob.driver.add_constraint('CL', equals=0.5)
+prob.driver.add_constraint('CL', equals=0.8)
 
 #setup data recording
 prob.driver.add_recorder(SqliteRecorder('weissinger.db'))
@@ -87,7 +89,8 @@ prob.setup()
 prob.run_once()
 
 if sys.argv[1] == '0':
-    prob.check_partial_derivatives(compact_print=True)
+    # prob.check_partial_derivatives(compact_print=True)
+    prob.check_total_derivatives()
     prob.run_once()
     print
     print prob['CL'], prob['CD']
@@ -95,3 +98,4 @@ elif sys.argv[1] == '1':
     prob.run()
     print prob['alpha'], prob['CL'], prob['CD']
     print prob['twist']
+    print prob['chord']
