7eeb8cbd5e9c6f4c50855b832019cb5e8d9c3f17
Justin Gray <justin.s.gray@gmail.com>
2016-03-21 03:03:56 -0400

    complex-step

--- prob1.py
+++ prob1.py
@@ -26,8 +26,8 @@ execfile('aluminum.py')
 
 # Define the loads
 loads = numpy.zeros((num_y, 6))
-loads[0, 2] = loads[-1, 2] = 1e4 # tip load of 1 kN
-# loads[:, 2] = 1e4 # load of 1 kN at each node
+# loads[0, 2] = loads[-1, 2] = 1e4 # tip load of 1 kN
+loads[:, 2] = 1e4 # load of 1 kN at each node
 
 span = 58.7630524 # [m] baseline CRM
 
@@ -71,7 +71,9 @@ prob.driver.add_desvar('t',
 prob.driver.add_objective('energy')
 prob.driver.add_constraint('weight', upper=1e5)
 
-# prob.root.fd_options['force_fd'] = True
+prob.root.fd_options['force_fd'] = True
+prob.root.fd_options['form'] = 'complex_step'
+prob.root.fd_options['step_size'] = 1e-10
 
 prob.driver.add_recorder(SqliteRecorder('spatialbeam.db'))
 
@@ -79,8 +81,9 @@ prob.setup()
 
 view_tree(prob, outfile="prob1.html", show_browser=False)
 
-prob.run_once()
-
+# prob.run_once()
 # prob.check_partial_derivatives(compact_print=True)
 
+st = time.time()
 prob.run()
+print "run time", time.time() - st
