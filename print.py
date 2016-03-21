7eebdec6e561c5ab411ce9cc6794fc69eee74a9c
Justin Gray <justin.s.gray@gmail.com>
2016-03-21 03:11:41 -0400

    added back some files. a little to zealous

---
+++ print.py
@@ -0,0 +1,32 @@
+### python print.py [as/a/s] [varname]
+
+import sqlitedict
+import sys
+import numpy
+
+
+def ndprint(a, format_string ='{0:9.2e}'):
+    print [format_string.format(v,i) for i,v in enumerate(a)]
+
+if sys.argv[1] == 'as':
+    filename = 'aerostruct'
+elif sys.argv[1] == 'a':
+    filename = 'weissinger'
+elif sys.argv[1] == 's':
+    filename = 'spatialbeam'
+else:
+    filename = sys.argv[1]
+varname = sys.argv[2]
+
+db = sqlitedict.SqliteDict(filename + '.db', 'openmdao')
+
+counter = 0
+for case_name, case_data in db.iteritems():
+    if "metadata" in case_name or "derivs" in case_name:
+        continue # don't plot these cases
+
+    print '%5i' % counter,
+#    print numpy.array_str(case_data['Unknowns'][varname], precision=3)
+    ndprint(case_data['Unknowns'][varname])
+
+    counter += 1
