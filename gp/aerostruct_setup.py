1488300f420271b8ea2e0f8d41a3a9f4c32c783f
Sam Friedman <samtx@outlook.com>
2016-07-11 17:33:36 -0500

    re-add aero_module.py and aerstruct_setup.py from before cleaning up git repo and remote branches

---
+++ gp/aerostruct_setup.py
@@ -0,0 +1,41 @@
+# Setup the aerostruct data using OpenMdDAO
+
+# In Matlab code, add this before calling Python functions:
+#   if count(py.sys.path,'') == 0
+#       insert(py.sys.path,int32(0),'');
+#   end
+
+from __future__ import division
+import numpy
+import sys
+
+# get OpenAeroStruct files from parent directory
+from pathlib import Path
+sys.path.append(str(Path('.').absolute().parent))
+
+from geometry import mesh_gen
+from spatialbeam import radii
+
+def setup(num_inboard=2, num_outboard=3):
+    # Create the mesh from inboard points and outboard points
+    mesh = mesh_gen(num_inboard, num_outboard)
+    num_x = mesh.shape[0]
+    num_y = mesh.shape[1]
+    r = radii(mesh)
+    t = r/10
+
+    # Define the aircraft properties
+    execfile('../CRM.py')  # from parent directory
+
+    des_vars = [
+        ('twist', numpy.zeros(num_y)),
+        ('span', span),
+        ('v', v),
+        ('alpha', alpha),
+        ('rho', rho),
+        ('disp', numpy.zeros((num_y, 6))),
+        ('r', r),
+        ('t', t),
+    ]
+
+    return {'mesh':mesh, 'num_x':num_x, 'num_y':num_y, 'des_vars':des_vars}
