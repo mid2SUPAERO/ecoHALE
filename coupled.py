796686df9f746ea3e7b3d3a51c065cfb06470bd2
Sam Friedman <samtx@outlook.com>
2016-07-21 19:42:57 -0500

    pass num_inboard and num_outboard args correctly in setup()

--- coupled.py
+++ coupled.py
@@ -32,7 +32,7 @@ def setup(num_inboard=2, num_outboard=3, check=False, out_stream=sys.stdout):
     # Create the mesh with 2 inboard points and 3 outboard points.
     # This will be mirrored to produce a mesh with 7 spanwise points,
     # or 6 spanwise panels
-    mesh = gen_crm_mesh(n_points_inboard=2, n_points_outboard=3, num_x=2)
+    mesh = gen_crm_mesh(n_points_inboard, n_points_outboard, num_x=2)
     num_x, num_y = mesh.shape[:2]
     num_twist = numpy.max([int((num_y - 1) / 5), 5])
     r = radii(mesh)
