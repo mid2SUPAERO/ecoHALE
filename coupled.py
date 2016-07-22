f5aee8123ef3871bb6136ede9601551c4e3b2de4
Sam Friedman <samtx@outlook.com>
2016-07-22 11:55:00 -0500

    fix num_inboard, num_outboard arguments

--- coupled.py
+++ coupled.py
@@ -32,7 +32,8 @@ def setup(num_inboard=2, num_outboard=3, check=False, out_stream=sys.stdout):
     # Create the mesh with 2 inboard points and 3 outboard points.
     # This will be mirrored to produce a mesh with 7 spanwise points,
     # or 6 spanwise panels
-    mesh = gen_crm_mesh(n_points_inboard, n_points_outboard, num_x=2)
+    print(type(num_inboard))
+    mesh = gen_crm_mesh(int(num_inboard), int(num_outboard), num_x=2)
     num_x, num_y = mesh.shape[:2]
     num_twist = numpy.max([int((num_y - 1) / 5), 5])
     r = radii(mesh)
