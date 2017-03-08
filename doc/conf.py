87fd3c4e9febba244b6c6e0ec8881205ec9149bd
johnjasa <johnjasa11@gmail.com>
2017-03-08 14:40:05 -0500

    Wrong doc theme

--- doc/conf.py
+++ doc/conf.py
@@ -118,21 +118,21 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'blue'
+html_theme = 'sphinxdoc'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    'source_link_position': "footer",
-    'bootswatch_theme': "flatly",
-    'navbar_sidebarrel': False,
-    'bootstrap_version': "3",
-    'navbar_links': [("API", "api"),
-                     ("Tutorial", "tutorial"),
-                     ("Gallery", "examples/index")],
-
-    }
+# html_theme_options = {
+#     'source_link_position': "footer",
+#     'bootswatch_theme': "flatly",
+#     'navbar_sidebarrel': False,
+#     'bootstrap_version': "3",
+#     'navbar_links': [("API", "api"),
+#                      ("Tutorial", "tutorial"),
+#                      ("Gallery", "examples/index")],
+#
+#     }
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
