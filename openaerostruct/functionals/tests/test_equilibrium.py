39c48f4928ffeb75a97a965204981e30f787c221
John T. Hwang <hwangjt@umich.edu>
2017-05-17 00:15:01 -0400

    Added equilibrium comp

--- openaerostruct/functionals/tests/test_equilibrium.py
+++ openaerostruct/functionals/tests/test_equilibrium.py
@@ -1,6 +1,6 @@
 import unittest
 
-from openaerostruct.functionals.breguet_range import BreguetRange
+from openaerostruct.functionals.equilibrium import Equilibrium
 from openaerostruct.utils.testing import run_test, get_default_prob_dict, get_default_surfaces
 
 
@@ -10,7 +10,7 @@ class Test(unittest.TestCase):
         surfaces = get_default_surfaces()
         prob_dict = get_default_prob_dict()
 
-        comp = BreguetRange(surfaces=surfaces, prob_dict=prob_dict)
+        comp = Equilibrium(surfaces=surfaces, prob_dict=prob_dict)
 
         run_test(self, comp)
 
--- openaerostruct/utils/testing.py
+++ openaerostruct/utils/testing.py
@@ -11,5 +11,24 @@ def run_test(obj, comp, decimal=3):
     prob.run_model()
     check = prob.check_partial_derivs(compact_print=True)
     for key, subjac in iteritems(check['']):
-        assert_almost_equal(subjac['rel error'].forward, 0., decimal=decimal, err_msg=key)
-        assert_almost_equal(subjac['rel error'].reverse, 0., decimal=decimal, err_msg=key)
+        if subjac['magnitude'].fd > 1e-6:
+            assert_almost_equal(subjac['rel error'].forward, 0., decimal=decimal, err_msg=key)
+            assert_almost_equal(subjac['rel error'].reverse, 0., decimal=decimal, err_msg=key)
+
+
+def get_default_prob_dict():
+    prob_dict = {
+        'g': 9.81,
+        'CT': 9.80665 * 17.e-6,
+        'a': 295.4,
+        'R': 14.3e6,
+        'M': 0.84,
+        'W0': 1e6,
+        'beta': 0.5,
+    }
+    return prob_dict
+
+
+def get_default_surfaces():
+    surfaces = [{'name': 'wing'}]
+    return surfaces
