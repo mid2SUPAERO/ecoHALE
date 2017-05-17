29aebc5abd50a14693ce5c5a03fdfb13a2860940
John T. Hwang <hwangjt@umich.edu>
2017-05-16 23:55:43 -0400

    Assert check deriv magnitude is small

--- openaerostruct/utils/testing.py
+++ openaerostruct/utils/testing.py
@@ -1,11 +1,15 @@
 from openmdao.api import Problem
 
-import unittest
+from six import iteritems
+from numpy.testing import assert_almost_equal
 
 
-def run_test(obj, comp):
+def run_test(obj, comp, decimal=3):
     prob = Problem(model=comp)
     prob.setup()
 
     prob.run_model()
     check = prob.check_partial_derivs(compact_print=True)
+    for key, subjac in iteritems(check['']):
+        assert_almost_equal(subjac['rel error'].forward, 0., decimal=decimal, err_msg=key)
+        assert_almost_equal(subjac['rel error'].reverse, 0., decimal=decimal, err_msg=key)
