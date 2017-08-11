ae4de7114587e6f3d8fccdb2eb5af1648c02d6fb
johnjasa <johnjasa11@gmail.com>
2017-08-11 15:25:34 -0400

    Docs

--- openaerostruct/doc/conf.py
+++ openaerostruct/doc/conf.py
@@ -23,7 +23,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-# needs_sphinx = '1.5.6'
+needs_sphinx = '1.5'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -34,7 +34,8 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode'
+    'sphinx.ext.viewcode',
+    'numpydoc'
 ]
 
 # Generate the API documentation when building
