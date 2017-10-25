ef8716bccbed70ed158f20cb7c8b20f0881c0f6d
John Jasa <johnjasa11@gmail.com>
2017-10-25 09:36:19 -0400

    Tried another setup method

--- setup.py
+++ setup.py
@@ -3,20 +3,41 @@ import os
 from subprocess import call
 
 
-setup(name='openaerostruct',
+# setup(name='openaerostruct',
+#     version='0.4.1',
+#     description='OpenAeroStruct',
+#     author='John Jasa',
+#     author_email='johnjasa@umich.edu',
+#     license='BSD-3',
+#     packages=[
+#         'openaerostruct',
+#         'openaerostruct/geometry',
+#         'openaerostruct/structures',
+#         'openaerostruct/aerodynamics',
+#         'openaerostruct/functionals',
+#         'openaerostruct/integration',
+#         'openaerostruct/fortran',
+#     ],
+#     # TODO: fix this with the correct requires
+#     install_requires=[],
+#     zip_safe=False,
+#     # ext_modules=ext,
+# )
+
+setup(name='openaerostruct_v2',
     version='0.4.1',
-    description='OpenAeroStruct',
+    description='OpenAeroStruct-v2',
     author='John Jasa',
     author_email='johnjasa@umich.edu',
     license='BSD-3',
     packages=[
-        'openaerostruct',
-        'openaerostruct/geometry',
-        'openaerostruct/structures',
-        'openaerostruct/aerodynamics',
-        'openaerostruct/functionals',
-        'openaerostruct/integration',
-        'openaerostruct/fortran',
+        'openaerostruct-v2',
+        'openaerostruct-v2/geometry',
+        'openaerostruct-v2/structures',
+        'openaerostruct-v2/aerodynamics',
+        # 'openaerostruct-v2/functionals',
+        # 'openaerostruct-v2/integration',
+        # 'openaerostruct-v2/fortran',
     ],
     # TODO: fix this with the correct requires
     install_requires=[],
