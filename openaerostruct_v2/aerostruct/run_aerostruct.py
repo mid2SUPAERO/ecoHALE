952ee365eb94eb157717708b5b6541828ade0e9b
John T. Hwang <hwangjt@umich.edu>
2017-11-16 00:20:18 -0500

    interm commit; working on AS opt

--- openaerostruct_v2/aerostruct/run_aerostruct.py
+++ openaerostruct_v2/aerostruct/run_aerostruct.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import numpy as np
 import matplotlib.pyplot as plt
 
-from openmdao.api import Problem, Group, IndepVarComp, pyOptSparseDriver, view_model
+from openmdao.api import Problem, Group, IndepVarComp, pyOptSparseDriver, view_model, ExecComp, SqliteRecorder
 
 from openaerostruct_v2.geometry.inputs_group import InputsGroup
 from openaerostruct_v2.structures.fea_bspline_group import FEABsplineGroup
@@ -16,27 +16,29 @@ from openaerostruct_v2.structures.fea_postprocess_group import FEAPostprocessGro
 from openaerostruct_v2.aerostruct.aerostruct_group import AerostructGroup
 
 
-num_nodes = 2
+num_nodes = 1
 
 num_points_x = 2
-num_points_z_half = 3
+num_points_z_half = 15
 num_points_z = 2 * num_points_z_half - 1
 lifting_surfaces = [
     ('wing', {
         'num_points_x': num_points_x, 'num_points_z_half': num_points_z_half,
         'airfoil': np.zeros(num_points_x),
         'chord': 1., 'twist': 0. * np.pi / 180., 'sweep_x': 0., 'dihedral_y': 0., 'span': 5,
-        'twist_bspline': (2, 2),
+        'twist_bspline': (6, 2),
         'sec_z_bspline': (num_points_z_half, 2),
         'chord_bspline': (2, 2),
-        'thickness_bspline': (10, 3),
-        'thickness' : .005,
-        'radius' : 0.1,
+        'thickness_bspline': (6, 3),
+        'thickness' : 0.02,
+        'radius' : 0.2,
         'distribution': 'sine',
         'section_origin': 0.25,
         'spar_location': 0.35,
         'E': 70.e9,
         'G': 29.e9,
+        'sigma_y': 200e6, #200e6,
+        'rho': 2700,
     })
 ]
 
@@ -78,10 +80,36 @@ prob.model.add_subsystem('fea_postprocess_group',
     promotes=['*'],
 )
 
+prob.model.add_subsystem('objective',
+    ExecComp('obj=10000 * sum(C_D) + structural_weight', C_D=np.zeros(num_nodes)),
+    promotes=['*'],
+)
+
+prob.model.add_design_var('alpha_rad', lower=-3.*np.pi/180., upper=8.*np.pi/180.)
+prob.model.add_design_var('wing_twist_dv', lower=-3.*np.pi/180., upper=8.*np.pi/180.)
+prob.model.add_design_var('wing_tube_thickness_dv', lower=0.001, upper=0.19, scaler=1e3)
+prob.model.add_objective('obj')
+prob.model.add_constraint('wing_ks', upper=0.)
+prob.model.add_constraint('C_L', equals=np.linspace(0.4, 0.6, num_nodes))
+
+prob.driver = pyOptSparseDriver()
+prob.driver.options['optimizer'] = 'SNOPT'
+prob.driver.opt_settings['Major optimality tolerance'] = 3e-7
+prob.driver.opt_settings['Major feasibility tolerance'] = 3e-7
+
+# prob.driver.add_recorder(SqliteRecorder('aerostruct.hst'))
+# prob.driver.recording_options['includes'] = ['*']
+
 prob.setup()
 
-prob.run_model()
-prob.check_partials(compact_print=True)
+prob['wing_chord_dv'] = [0.5, 1.0, 0.5]
+
+if 0:
+    prob.run_model()
+    prob.check_partials(compact_print=True)
+    exit()
+
+prob.run_driver()
 
 # print(prob['circulations'])
 # print(prob['wing_disp'])
