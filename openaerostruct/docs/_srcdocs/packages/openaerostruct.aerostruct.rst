31f588f147faf87019c6a1cb6bac17096b22f218
John Jasa <johnjasa@umich.edu>
2018-02-12 20:31:28 -0500

    Trying to figure out RTD path

--- openaerostruct/docs/_srcdocs/packages/openaerostruct.aerostruct.rst
+++ openaerostruct/docs/_srcdocs/packages/openaerostruct.aerostruct.rst
@@ -5,5 +5,6 @@ openaerostruct.aerostruct
     :maxdepth: 1
 
     aerostruct/aerostruct_group
+    aerostruct/aerostruct_postprocess_group
     aerostruct/disp_transfer_group
     aerostruct/load_transfer_group
--- openaerostruct/docs/conf.py
+++ openaerostruct/docs/conf.py
@@ -74,6 +74,8 @@ sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('./_exts'))
 
+print(sys.path)
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
