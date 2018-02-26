5702901e45d1f4184510bfc547dc354d091e19c8
johnjasa <johnjasa11@gmail.com>
2018-02-26 16:32:32 -0500

    Curious what happens if no master_doc

--- openaerostruct/docs/conf.py
+++ openaerostruct/docs/conf.py
@@ -114,7 +114,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'index'
+# master_doc = 'index'
 
 # General information about the project.
 project = u'OpenAeroStruct'
