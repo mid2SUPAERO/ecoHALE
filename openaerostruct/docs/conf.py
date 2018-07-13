5b007f61feacee0290de4ef631cb601d1f3ff46b
kmarsteller <keith.g.marsteller@nasa.gov>
2018-07-13 09:51:36 -0400

    adding local extensions

--- openaerostruct/docs/conf.py
+++ openaerostruct/docs/conf.py
@@ -9,9 +9,9 @@ from openmdao.docs.config_params import MOCK_MODULES
 from openaerostruct.docs._utils.patch import do_monkeypatch
 from openaerostruct.docs._utils.generate_sourcedocs import generate_docs
 
-import openmdao
-openmdao_path = os.path.split(os.path.abspath(openmdao.__file__))[0]
-sys.path.insert(0, os.path.join(openmdao_path, 'docs', '_exts'))
+import openaerostruct
+openaerostruct_path = os.path.split(os.path.abspath(openaerostruct.__file__))[0]
+sys.path.insert(0, os.path.join(openaerostruct_path, 'docs', '_exts'))
 
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('.'))
