565856d7a89f3b84925b56a17cf549b4f3af7349
cmader <cmader@umich.edu>
2018-08-14 13:38:33 -0400

    updated the von mises test to fix the nans

--- openaerostruct/structures/tests/test_vonmises_tube.py
+++ openaerostruct/structures/tests/test_vonmises_tube.py
@@ -1,5 +1,7 @@
 import unittest
+import numpy as np
 
+from openmdao.api import Group, IndepVarComp
 from openaerostruct.structures.vonmises_tube import VonMisesTube
 from openaerostruct.utils.testing import run_test, get_default_surfaces
 
@@ -9,9 +11,36 @@ class Test(unittest.TestCase):
     def test(self):
         surface = get_default_surfaces()[0]
 
+        group = Group()
+
         comp = VonMisesTube(surface=surface)
 
-        run_test(self, comp)
+        indep_var_comp = IndepVarComp()
+
+        ny = surface['num_y']
+
+        #  define the spar with y out the wing
+        nodes = np.zeros((ny, 3))
+        nodes[:,1] = np.linspace(0,1,ny)
+
+        radius = 0.01*np.ones((ny - 1))
+
+        disp = np.zeros((ny, 6))
+        for i in range(6):
+            disp[:,i] = np.linspace(0,0.001,ny)
+        
+        indep_var_comp.add_output('nodes', val=nodes, units='m')
+        indep_var_comp.add_output('radius', val=radius, units='m')
+        indep_var_comp.add_output('disp', val=disp, units='m')
+
+        group.add_subsystem('vm_comp', comp)
+        group.add_subsystem('indep_var_comp', indep_var_comp)
+
+        group.connect('indep_var_comp.nodes', 'vm_comp.nodes')
+        group.connect('indep_var_comp.radius', 'vm_comp.radius')
+        group.connect('indep_var_comp.disp', 'vm_comp.disp')
+
+        run_test(self, group)
 
 
 if __name__ == '__main__':
