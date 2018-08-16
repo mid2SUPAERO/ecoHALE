a767d5dec3bbdbd288cce794c483cd32179239aa
Neil Wu <neilwu@umich.edu>
2018-08-16 12:49:53 -0400

    updated monotonic constraint to use const sparse jac, and added associated tests

--- openaerostruct/geometry/monotonic_constraint.py
+++ openaerostruct/geometry/monotonic_constraint.py
@@ -37,7 +37,23 @@ class MonotonicConstraint(ExplicitComponent):
         self.add_input(self.var_name, val=np.zeros(self.ny))
         self.add_output(self.con_name, val=np.zeros(self.ny-1))
 
-        self.declare_partials('*', '*')
+        rows = np.arange(0,self.ny-1)
+        rows = np.vstack((rows,rows)).flatten(order='F')
+
+        cols = np.arange(1,self.ny-1)
+        cols = np.vstack((cols,cols)).flatten(order='F')
+        cols = np.insert(cols,0,0)
+        cols = np.append(cols,self.ny-1)
+
+        sparse_val = np.ones_like(rows)
+        sparse_val[1::2] = -1
+        if not self.symmetry:
+            if self.ny%2 == 0:
+                sparse_val[self.ny-2:] *= -1
+            else:
+                sparse_val[self.ny-1:] *= -1
+
+        self.declare_partials(self.con_name, self.var_name, rows=rows,cols=cols,val=sparse_val)
 
     def compute(self, inputs, outputs):
         # Compute the difference between adjacent variable values
@@ -48,11 +64,3 @@ class MonotonicConstraint(ExplicitComponent):
             ny2 = (self.ny - 1) // 2
             outputs[self.con_name][:ny2] = diff[:ny2]
             outputs[self.con_name][ny2:] = -diff[ny2:]
-
-    def compute_partials(self, inputs, partials):
-        np.fill_diagonal(partials[self.con_name, self.var_name][:, :], 1)
-        np.fill_diagonal(partials[self.con_name, self.var_name][:, 1:], -1)
-
-        if not self.symmetry:
-            ny2 = (self.ny - 1) // 2
-            partials[self.con_name, self.var_name][ny2:, :] *= -1
