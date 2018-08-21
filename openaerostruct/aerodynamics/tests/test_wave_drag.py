6d930987d32dccc8a7dc426d5abcb7c747de5eee
shamsheersc19 <sschau@umich.edu>
2018-08-20 23:56:06 -0400

    fixing wave drag derivs

--- openaerostruct/aerodynamics/tests/test_wave_drag.py
+++ openaerostruct/aerodynamics/tests/test_wave_drag.py
@@ -19,10 +19,21 @@ class Test(unittest.TestCase):
         group = Group()
 
         indep_var_comp = IndepVarComp()
-        indep_var_comp.add_output('t_over_c', val=np.arange(ny-1))
+        indep_var_comp.add_output('t_over_c_cp', val=surface['t_over_c_cp'])
+        indep_var_comp.add_output('M', val=.95)
+        indep_var_comp.add_output('CL', val=0.7)
+        indep_var_comp.add_output('widths', val = np.array([12.14757848, 11.91832712, 11.43730892]))
+        indep_var_comp.add_output('cos_sweep', val = np.array([10.01555924,  9.80832351,  9.79003729]))
+        indep_var_comp.add_output('chords', val = np.array([ 2.72835132,  5.12528179,  7.88916016, 13.6189974]))
         group.add_subsystem('indep_var_comp', indep_var_comp, promotes=['*'])
 
-        comp = WaveDrag(surface=surface, with_wave=True)
+        group.add_subsystem('t_over_c_bsp', BsplinesComp(
+            in_name='t_over_c_cp', out_name='t_over_c',
+            num_control_points=n_cp, num_points=int(ny-1),
+            bspline_order=min(n_cp, 4), distribution='uniform'),
+            promotes_inputs=['t_over_c_cp'], promotes_outputs=['t_over_c'])
+
+        comp = WaveDrag(surface=surface)
         group.add_subsystem('wavedrag', comp, promotes=['*'])
 
         run_test(self, group, complex_flag=True)
