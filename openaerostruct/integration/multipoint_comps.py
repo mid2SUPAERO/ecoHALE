5a475b1bdf3c337d613f72e506c98c84f4867be1
johnjasa <johnjasa11@gmail.com>
2018-08-30 11:58:49 -0400

    Removed unnecessary component

--- openaerostruct/integration/multipoint_comps.py
+++ openaerostruct/integration/multipoint_comps.py
@@ -19,27 +19,3 @@ class MultiCD(ExplicitComponent):
         outputs['CD'] = 0.
         for i in range(self.n_points):
             outputs['CD'] += inputs[str(i) + '_CD']
-
-class GeomMatch(ExplicitComponent):
-
-    def initialize(self):
-        self.options.declare('n_points', types=int)
-        self.options.declare('mx', types=int)
-        self.options.declare('my', types=int)
-
-    def setup(self):
-        self.n_points = self.options['n_points']
-        self.mx = self.options['mx']
-        self.my = self.options['my']
-        for i in range(self.n_points):
-            self.add_input(str(i) + '_shape', val=np.zeros((self.mx, self.my)), units='m')
-
-        self.add_output('shape_diff', val=np.zeros((self.mx, self.my * (self.n_points - 1))), units='m')
-
-        self.declare_partials('*', '*')
-
-    def compute(self, inputs, outputs):
-        shape_0 = inputs['0_shape']
-
-        for i in range(1, self.n_points):
-            outputs['shape_diff'][:, (i-1)*self.my:i*self.my] = inputs[str(i) + '_shape'] - shape_0
