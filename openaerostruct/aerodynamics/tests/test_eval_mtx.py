a1a0b5942d6c73d6d2e099a698f83c53dffe508b
John Jasa <johnjasa@umich.edu>
2018-09-01 01:18:38 -0400

    Slightly decreased time for duplicate eval_mtx indexing

--- openaerostruct/aerodynamics/tests/test_eval_mtx.py
+++ openaerostruct/aerodynamics/tests/test_eval_mtx.py
@@ -1,4 +1,6 @@
 import unittest
+from openmdao.api import Problem
+from openmdao.utils.assert_utils import assert_check_partials
 
 from openaerostruct.aerodynamics.eval_mtx import EvalVelMtx
 from openaerostruct.utils.testing import run_test, get_default_surfaces
@@ -13,6 +15,24 @@ class Test(unittest.TestCase):
 
         run_test(self, comp)
 
+    def test_assembled_jac(self):
+        surfaces = get_default_surfaces()
+
+        comp = EvalVelMtx(surfaces=surfaces, num_eval_points=2, eval_name='test_name')
+
+        prob = Problem()
+        prob.model.add_subsystem('comp', comp)
+
+        from openmdao.api import DirectSolver
+        prob.model.linear_solver = DirectSolver(assemble_jac=True)
+        prob.model.options['assembled_jac_type'] = 'csc'
+
+        prob.setup()
+
+        prob.run_model()
+
+        data = prob.check_partials(compact_print=True, out_stream=None, method='cs', step=1e-40)
+        assert_check_partials(data, atol=1e20, rtol=1e-6)
 
 if __name__ == '__main__':
     unittest.main()
