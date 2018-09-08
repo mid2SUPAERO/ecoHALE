84365727dc679f94444edc0d559d92697c19189d
Kenneth Moore <kenneth.t.moore-1@nasa.gov>
2018-09-07 20:37:01 -0400

    moment derivatives complete

--- openaerostruct/functionals/moment_coefficient.py
+++ openaerostruct/functionals/moment_coefficient.py
@@ -59,7 +59,6 @@ class MomentCoefficient(ExplicitComponent):
         self.add_output('CM', val=np.ones((3)))
 
         self.declare_partials(of='*', wrt='*')
-        #self.declare_partials('*', '*', method='cs')
 
     def compute(self, inputs, outputs):
         cg = inputs['cg']
@@ -164,6 +163,7 @@ class MomentCoefficient(ExplicitComponent):
 
             dMAC_dc = (2.0 / S_ref) * np.einsum('i,ij', panel_chords * widths, dpc_dc)
             dMAC_dw = (1.0 / S_ref) * panel_chords**2
+            dMAC_dS = -MAC / S_ref
 
             # If the surface is symmetric, then the previously computed MAC
             # is half what it should be
@@ -171,12 +171,14 @@ class MomentCoefficient(ExplicitComponent):
                 MAC *= 2.0
                 dMAC_dc *= 2.0
                 dMAC_dw *= 2.0
+                dMAC_dS *= 2.0
 
             # diff derivs
             pts = (b_pts[:, 1:, :] + b_pts[:, :-1, :]) * 0.5
             diff = (pts - cg)
 
             c = np.cross(diff, sec_forces, axis=2)
+            moment = np.sum(c, axis=0)
 
             dcda = np.zeros((3, nx-1, ny-1, 3))
             dcda[0, :, :, 1] = sec_forces[:, :, 2]
@@ -204,6 +206,9 @@ class MomentCoefficient(ExplicitComponent):
             # If the surface is symmetric, set the x- and z-direction moments
             # to 0 and double the y-direction moment
             if surface['symmetry']:
+                moment[:, 0] = 0.
+                moment[:, 1] *= 2.0
+                moment[:, 2] = 0.
                 partials['CM', name + '_sec_forces'][0, :] = 0.
                 partials['CM', name + '_sec_forces'][1, :] *= 2.0
                 partials['CM', name + '_sec_forces'][2, :] = 0.
@@ -216,6 +221,25 @@ class MomentCoefficient(ExplicitComponent):
 
             partials['CM', 'cg'] -= dcda * fact / MAC
 
-            # MAC derivative only comes from first surface.
+            M_j = np.sum(moment, axis=0)
+            term = fact / MAC**2
+            partials['CM', name + '_chords'] = -np.outer(M_j * term, dMAC_dc)
+            partials['CM', name + '_widths'] = -np.outer(M_j * term, dMAC_dw)
+            partials['CM', name + '_S_ref'] = -np.outer(M_j, dMAC_dS * term)
+
+            # For first surface, MAC is a higher power.
             if j == 0:
-                pass
+                partials['CM', name + '_chords'] *= 2.0
+                partials['CM', name + '_widths'] *= 2.0
+                partials['CM', name + '_S_ref'] *= 2.0
+                base_name = name
+                base_dMAC_dc = dMAC_dc
+                base_dMAC_dw = dMAC_dw
+                base_dMAC_dS = dMAC_dS
+
+            else:
+                # Apply this surface's portion of the moment to the MAC_wing term.
+                term = fact / (MAC_wing * MAC)
+                partials['CM', base_name + '_chords'] -= np.outer(M_j * term, base_dMAC_dc)
+                partials['CM', base_name + '_widths'] -= np.outer(M_j * term, base_dMAC_dw)
+                partials['CM', base_name + '_S_ref'] -= np.outer(M_j, base_dMAC_dS * term)
