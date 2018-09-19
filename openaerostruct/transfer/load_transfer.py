b23ca3cf2e447042c7b863bed9c5aced6e62d579
johnjasa <johnjasa11@gmail.com>
2018-09-19 17:05:45 -0400

    Trying dense on one multiplication

--- openaerostruct/transfer/load_transfer.py
+++ openaerostruct/transfer/load_transfer.py
@@ -339,11 +339,11 @@ class LoadTransfer(ExplicitComponent):
 
         # multiply the matrix with those previous calculated in setup
         dloadsB__ddef_mesh = self.dloadsB__dmoment * dmoment__ddiff * self.ddiff__ddef_mesh
-        # dloadsB__ddef_mesh = dloadsB__ddef_mesh.tocoo()
+        dloadsB__ddef_mesh = dloadsB__ddef_mesh.tocoo()
         #J['loadsB','def_mesh'] = dloadsB__ddef_mesh.data
 
-        dloads__ddef_mesh = self.dloads__dloadsB * dloadsB__ddef_mesh
-        # dloads__ddef_mesh = dloads__ddef_mesh.tocoo()
+        dloads__ddef_mesh = self.dloads__dloadsB.todense() * dloadsB__ddef_mesh.todense()
+        dloads__ddef_mesh = dloads__ddef_mesh.tocoo()
         J['loads','def_mesh'] = dloads__ddef_mesh.data
 
         dloads__dsec_forces = self.dloads__dloadsA__dsec_forces + self.dloads__dloadsB * dloadsB__dsec_forces
