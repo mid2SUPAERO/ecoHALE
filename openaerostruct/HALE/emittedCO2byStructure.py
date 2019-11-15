2318fb07de3ab6c3ca16d0cfcd046f7ea34142df
Edouard DURIEZ <e.duriez@ad.isae-supaero.fr>
2019-11-15 17:15:26 +0100

    added files needed to run hale optimisation

---
+++ openaerostruct/HALE/emittedCO2byStructure.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 13 17:28:06 2019
+
+@author: e.duriez
+"""
+
+from __future__ import print_function
+import numpy as np
+import math
+from openmdao.api import ExplicitComponent
+
+from fctMultiMatos import*
+
+
+
+class structureCO2(ExplicitComponent):
+
+    def initialize(self):
+        self.options.declare('surfaces', types=list)
+
+    def setup(self):
+
+        self.add_input('mrho', val=1000, units='kg/m**3') #ED
+        self.add_input('mass', val=100, units='kg')#ED
+        self.add_input('PV_mass', val=100, units='kg')#ED
+        
+        #code added for debug
+#        self.ny=len(surface['t_over_c_cp']) #TODELETE
+#        self.add_input('twist', val=np.zeros((self.ny))) #TODELETE
+#        self.add_input('spar_thickness', val=np.zeros((self.ny))) #TODELETE
+#        self.add_input('skin_thickness', val=np.zeros((self.ny))) #TODELETE
+#        self.add_input('span', val=5) #TODELETE
+#        self.add_input('chord', val=2) #TODELETE
+#        self.add_input('taper', val=0.1) #TODELETE
+#        self.add_input('t_over_c', val=np.zeros((self.ny))) #TODELETE
+#        self.add_input('alpha_maneuver', val=1) #TODELETE
+#        self.add_input('mrhoVar', val=1700) #TODELETE
+
+        self.add_output('emitted_co2', val=20000, units='kg')#ED
+
+        self.declare_partials('emitted_co2', 'mass')
+        self.declare_partials('emitted_co2', 'PV_mass')
+#        self.declare_partials('emitted_co2', 'mrho', method='fd', step=0.1, step_calc='abs')
+        self.declare_partials('emitted_co2', 'mrho', method='cs')
+
+    def compute(self, inputs, outputs):
+        surfaces = self.options['surfaces']
+        for surface in surfaces:
+            puissanceMM = surface['puissanceMM']
+            materlist=surface['materlist']
+            PVco2=surface['co2PV']
+
+#        print('structCO2') #ED
+#        print(inputs['mrho'])  #ED
+#        print(inputs['mass'])
+#        if math.floor(abs(inputs['mass']))==31116:  #TODELETE
+#            print('there we are')  #TODELETE
+        
+
+        co2 = co2MM(inputs['mrho'],materlist,puissanceMM)  #ED
+        outputs['emitted_co2']=co2*inputs['mass']+PVco2*inputs['PV_mass']
+
+    def compute_partials(self, inputs, partials):
+        surfaces = self.options['surfaces']
+        for surface in surfaces:
+            puissanceMM = surface['puissanceMM']
+            materlist=surface['materlist']
+            PVco2=surface['co2PV']
+
+        co2 = co2MM(inputs['mrho'],materlist,puissanceMM)  #ED        
+
+
+        partials['emitted_co2', 'mass']=co2
+        partials['emitted_co2', 'PV_mass']=PVco2
---
+++ openaerostruct/HALE/seeOptim.py
@@ -0,0 +1,217 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 26 17:06:34 2019
+
+@author: e.duriez
+"""
+from openmdao.api import Problem, ScipyOptimizeDriver, SqliteRecorder, CaseReader
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+cr = CaseReader("aerostructMrhoi396p1limh8220.db")
+
+driver_cases = cr.list_cases('driver')
+
+iterations=len(driver_cases)
+#$iterations=3
+
+mrho=[]
+masse=[]
+co2=[]
+taper=[]
+span=[]
+chord=[]
+chordTip=[]
+surf=[]
+surface0=[]
+surface1=[]
+sparThicknessRoot=[]
+sparThicknessTip=[]
+skinThicknessRoot=[]
+skinThicknessTip=[]
+failure=[]
+power=[]
+lift=[]
+tOverC1=[]
+tOverC2=[]
+buckling=[]
+
+for i in range(iterations):
+#for i in range(350,380):
+    case = cr.get_case(driver_cases[i])
+    design_vars = case.get_design_vars()
+    objective= case.get_objectives()
+    constraints= case.get_constraints()
+    mrho.append(design_vars['mrho'][0])
+    masse.append(case.outputs['wing.structural_mass'][0])
+    co2.append(objective['emitted_co2'][0])
+    taper.append(case.inputs['wing.geometry.mesh.taper.taper'][0])
+    span.append(case.inputs['wing.geometry.mesh.stretch.span'][0])
+    chord.append(case.inputs['wing.geometry.mesh.scale_x.chord'][0])
+    chordTip.append(case.inputs['wing.geometry.mesh.scale_x.chord'][-1])
+    surface0.append(case.outputs['AS_point_0.coupled.wing.S_ref'][0])
+#    surface1.append(case.outputs['AS_point_1.coupled.wing.S_ref'][0])
+    sparThicknessRoot.append(design_vars['wing.spar_thickness_cp'][-1])
+    sparThicknessTip.append(design_vars['wing.spar_thickness_cp'][0])
+    skinThicknessRoot.append(design_vars['wing.skin_thickness_cp'][-1])
+    skinThicknessTip.append(design_vars['wing.skin_thickness_cp'][0])
+#    failure.append(constraints['AS_point_1.wing_perf.failure'][0])
+#    power.append(constraints['AS_point_1.enough_power'][0])
+#    lift.append(constraints['AS_point_1.L_equals_W'][0])
+    failure.append(constraints['AS_point_1.wing_perf.failure'][0])
+    power.append(constraints['AS_point_0.enough_power'][0])
+    lift.append(constraints['AS_point_0.L_equals_W'][0])
+    tOverC1.append(case.outputs['wing.geometry.t_over_c_cp'])
+    tOverC2.append(case.outputs['wing.t_over_c'][0])
+    buckling.append(constraints['AS_point_1.wing_perf.buckling'][0])
+
+chordEnd=np.multiply(chord,taper)
+doublemeanchord=np.add(chord,chordEnd)
+meanchord=[x/2 for x in doublemeanchord]
+surf=np.multiply(meanchord,span)
+
+
+
+print(mrho)
+#print(masse)
+print(co2)
+
+plt.semilogy(masse)
+plt.xlabel('iteration')
+plt.ylabel('mass')
+#plt.xlim((0,150))
+
+plt.show()
+
+plt.plot(mrho)
+plt.xlabel('iteration')
+plt.ylabel('mrho')
+#plt.xlim((0,150))
+
+plt.show()
+
+#/plt.plot(co2)
+plt.semilogy(co2)
+plt.xlabel('iteration')
+plt.ylabel('co2')
+#plt.xlim((0,150))
+
+plt.show()
+
+plt.plot(taper)
+plt.xlabel('iteration')
+plt.ylabel('taper')
+#plt.xlim((0,150))
+plt.ylim((0,0.2))
+
+plt.show()
+
+plt.plot(span)
+plt.xlabel('iteration')
+plt.ylabel('span')
+#plt.xlim((0,150))
+
+plt.show()
+
+plt.plot(chord)
+plt.xlabel('iteration')
+plt.ylabel('chord')
+#plt.xlim((0,150))
+#plt.ylim((1,1.5))
+
+plt.show()
+
+plt.plot(skinThicknessRoot)
+plt.plot(skinThicknessTip)
+plt.xlabel('iteration')
+plt.ylabel('skin')
+#plt.xlim((250,300))
+plt.ylim((0,4))
+
+plt.show()
+
+plt.plot(sparThicknessRoot)
+plt.plot(sparThicknessTip)
+plt.xlabel('iteration')
+plt.ylabel('spar')
+#plt.xlim((250,300))
+plt.ylim((0,4))
+
+plt.show()
+
+plt.plot(failure)
+plt.xlabel('iteration')
+plt.ylabel('failure')
+#plt.xlim((250,300))
+plt.ylim((-1,1))
+
+plt.show()
+
+plt.plot(power)
+plt.xlabel('iteration')
+plt.ylabel('power')
+#plt.xlim((250,300))
+#plt.ylim((0,4))
+
+plt.show()
+
+plt.plot(lift)
+plt.xlabel('iteration')
+plt.ylabel('lift')
+#plt.xlim((250,300))
+#plt.ylim((0,4))
+
+plt.show()
+
+plt.plot(tOverC1)
+plt.xlabel('iteration')
+plt.ylabel('toverc1')
+#plt.xlim((250,300))
+#plt.ylim((0,4))
+
+plt.show()
+
+plt.plot(tOverC2)
+plt.xlabel('iteration')
+plt.ylabel('toverc2')
+#plt.xlim((250,300))
+#plt.ylim((0,4))
+
+plt.show()
+
+plt.plot(buckling)
+plt.xlabel('iteration')
+plt.ylabel('buckling')
+#plt.xlim((250,300))
+plt.ylim((-1,1))
+
+plt.show()
+
+#plt.plot(chordTip)
+#plt.xlabel('iteration')
+#plt.ylabel('chord at wing tip')
+#plt.xlim((0,150))
+#
+#plt.show()
+#
+#plt.plot(surf)
+#plt.xlabel('iteration')
+#plt.ylabel('surface aprox')
+#plt.xlim((0,150))
+#
+#plt.show()
+#
+#plt.plot(surface0)
+#plt.xlabel('iteration')
+#plt.ylabel('surface point0')
+#plt.xlim((0,150))
+#
+#plt.show()
+#
+#plt.plot(surface1)
+#plt.xlabel('iteration')
+#plt.ylabel('surface point1')
+#plt.xlim((0,150))
+#
+#plt.show()
